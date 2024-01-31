139d58e8f25af030d0f4f51ac77e5cfbd3b0985a
Cov3n4nt <erosval126@gmail.com>
2024-01-31 23:07:36 +0800

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,6 @@ export default function Home() {
         <section className={utilStyles.headingMd}>
           <p>Hi, I'm a Soft Engineer Intern from Angeles University Foundation with a keen focus in Native Android development. Stepping into twist resources, I'm met with a cohesive and accommodating work environment.
               I'm eager to enhance my skills and delve into Web Development, gaining valuable real-world experience along the way. </p>
-          <p>
-            (This is a sample website - you’ll be building a site like this on{' '}
-            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-          </p>
         </section>
       </Layout>
   );
